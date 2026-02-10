09987b3d9fda606c3d3df8f08b20f0e587e268a8
Peter Vatnar Andersson <peterva@uia.no>
2026-02-10 10:39:51 +0100

    Fix wrong function

--- StringLibrary/StringLibrary/Class1.cs
+++ StringLibrary/StringLibrary/Class1.cs
@@ -6,8 +6,8 @@ public static class StringLibrary
     public static bool StartsWithUpper(this string? str)
     {
         if (string.IsNullOrWhiteSpace(str))
-            return true;
+            return false;
 
-        return !char.IsUpper(str[0]);
+        return char.IsUpper(str[0]);
     }
 }
